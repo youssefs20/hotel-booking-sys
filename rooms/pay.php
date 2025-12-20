3900d77c2499e63a2b4eb906063f4a1b15644618
youssef sameh <114283304+youssefs20@users.noreply.github.com>
2025-12-20 15:12:30 +0200

    Bookings (#4)
    * paying with paypal
    
    * finished payment

--- rooms/pay.php
+++ rooms/pay.php
@@ -1,31 +1,43 @@
 <?php require "../includes/header.php"; ?> 
-<?php require "../config/config.php"; ?> 
+<?php require "../config/config.php"; ?>
 
-<div class="container">  
-    <!-- Replace "test" with your own sandbox Business account app client ID -->
-    <script src="https://www.paypal.com/sdk/js?client-id=AW9ZekcfSeOcQ12rgAef4J8RjWuqQaHYG1v3FmGh_et4WVI4gXgDhbZN6V-yvXzB195QBNOgMtnq1IEY&currency=USD"></script>
-    <!-- Set up a container element for the button -->
-    <div id="paypal-button-container"></div>
-    <script>
-        paypal.Buttons({
-        // Sets up the transaction when a payment button is clicked
-        createOrder: (data, actions) => {
-            return actions.order.create({
-            purchase_units: [{
-                amount: {
-                value: '<?php echo $_SESSION['price']; ?>' // Can also reference a variable or function
-                }
-            }]
-            });
-        },
-        // Finalize the transaction after payer approval
-        onApprove: (data, actions) => {
-            return actions.order.capture().then(function(orderData) {
-                window.location.href='http://localhost/hotel-booking/';
-            });
-        }
-        }).render('#paypal-button-container');
-    </script>
+<div class="hero-wrap js-fullheight" style="background-image: url('<?php echo APPURL; ?>/images/image_2.jpg');" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+            <div class="col-md-7 ftco-animate">
+                <h2 class="subheading">pay page for your room</h2>
+                <div class="container">  
+                    <!-- Replace "test" with your own sandbox Business account app client ID -->
+                    <script src="https://www.paypal.com/sdk/js?client-id=AW9ZekcfSeOcQ12rgAef4J8RjWuqQaHYG1v3FmGh_et4WVI4gXgDhbZN6V-yvXzB195QBNOgMtnq1IEY&currency=USD"></script>
+                    <!-- Set up a container element for the button -->
+                    <div id="paypal-button-container"></div>
+                    <script>
+                        paypal.Buttons({
+                        // Sets up the transaction when a payment button is clicked
+                        createOrder: (data, actions) => {
+                            return actions.order.create({
+                            purchase_units: [{
+                                amount: {
+                                value: '<?php echo $_SESSION['price']; ?>' // Can also reference a variable or function
+                                }
+                            }]
+                            });
+                        },
+                        // Finalize the transaction after payer approval
+                        onApprove: (data, actions) => {
+                            return actions.order.capture().then(function(orderData) {
+                                window.location.href='http://localhost/hotel-booking/';
+                            });
+                        }
+                        }).render('#paypal-button-container');
+                    </script>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
 
 <?php require "../includes/footer.php"; ?> 
